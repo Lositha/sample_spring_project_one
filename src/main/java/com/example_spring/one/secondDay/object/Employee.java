dac3fda55880c018df0cb32ca455763286737e3a
LalithK90 <asakahatapitiya@gmail.com>
2020-12-30 20:03:01 +0530

    Employee object was created

---
+++ src/main/java/com/example_spring/one/secondDay/object/Employee.java
@@ -0,0 +1,16 @@
+package com.example_spring.one.secondDay.object;
+
+import lombok.*;
+
+@ToString
+@EqualsAndHashCode
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+  private int id;
+  private String name;
+  private String mobile;
+
+}
